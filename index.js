52ab50d755d035938d071f2dfbd73761711cd305
rakennus <rakennusmf@gmail.com>
2024-10-23 19:46:49 +0200

    much better table using css grid

--- index.js
+++ index.js
@@ -1,93 +1,97 @@
-document.getElementById('csvFile').addEventListener('change', function (event) {
+const parent = document.getElementById('table')
+const tableStyle = document.getElementById("tableStyle")
+
+var container = document.getElementById("typeSelect")
+var checkboxesAll = container.querySelectorAll('input[type="checkbox"]');
+
+checkboxesAll.forEach(element => {
+    element.addEventListener("click", function (test) {
+        console.log({state: test.target.checked, value: test.target.value});
+    })
+});
+
+document.getElementById('csv_input').addEventListener('change', function (event) {
     const file = event.target.files[0];
-    if (!file) {
-        return;
-    }
+
+    if (!file) return
+
     const reader = new FileReader();
+
     reader.onload = function (e) {
         csvData = e.target.result;
         const separator = detectSeparator(csvData);
-        let array = CSVToArray(csvData, separator)
+        let csvArray = CSVToArray(csvData, separator)
 
-        let newArray = [];
+        let objArray = [];
 
-        for (let i = 1; i < array.length - 1; i++) {
+        for (let i = 1; i < csvArray.length - 1; i++) {
             let obj = {};
-            for (let j = 0; j < array[0].length; j++) {
-                obj[array[0][j]] = array[i][j];
+            for (let j = 0; j < csvArray[0].length; j++) {
+                obj[csvArray[0][j]] = csvArray[i][j];
             }
-            newArray.push(obj)
+            objArray.push(obj)
         }
 
-        let weekdays = ['MO', 'DI', 'MI', 'DO', 'FR']
-
-        let week = [
-            [],
-            [],
-            [],
-            [],
-            []
-        ]
-
-        newArray.forEach(element => {
-            week[weekdays.indexOf(element.WOCHENTAG)].push(element)
-        });
-
-        console.log(newArray)
-        console.log(week)
-
-        const div = document.createElement('div')
-        div.classList.add("parent")
-
-        weekdays.forEach((element, index) => {
-            const dayDiv = document.createElement('div')
-            dayDiv.setAttribute("id", element);
-            dayDiv.classList.add("child")
-            
-            week[index].forEach((event, rowIndex) => {
-                let eventDiv = document.createElement('div')
-                eventDiv.classList.add("event")
-                eventDiv.classList.add(event.LV_ART)
-
-                let p1 = document.createElement('p');
-                p1.textContent = event.TITEL;
-                eventDiv.appendChild(p1)
-
-                let p2 = document.createElement('p');
-                p2.textContent = event.WOCHENTAG;
-                eventDiv.appendChild(p2)
-
-                dayDiv.appendChild(eventDiv);
-            });
-            div.appendChild(dayDiv)
-        });
-
-        document.body.insertBefore(div, document.body.childNodes[0]);
-
-
-        // displayArray(array);
+        displayArray(objArray);
     };
     reader.readAsText(file, 'ISO-8859-4');
 });
 
-/* function displayArray(array) {
-    const table = document.getElementById('csvTable');
-    table.innerHTML = ''; // Clear the table first
+function timeStringToFloat(time) {
+    var hoursMinutes = time.split(/[.:]/);
+    var hours = parseInt(hoursMinutes[0], 10);
+    var minutes = hoursMinutes[1] ? parseInt(hoursMinutes[1], 10) : 0;
+    return hours + minutes / 60;
+}
 
+function displayArray(array) {
+    parent.innerHTML = '';
 
-    // Loop through rows and add them to the table
-    array.forEach((row, rowIndex) => {
-        const tr = document.createElement('tr');
+    array.forEach((event, rowIndex) => {
+        let eventDiv = document.createElement('div')
+        eventDiv.classList.add("event")
+        eventDiv.classList.add(event.LV_ART)
+        eventDiv.classList.add(event.WOCHENTAG)
+        eventDiv.classList.add("time" + (timeStringToFloat(event.VON) - 8) * 2)
 
-        row.forEach(element => {
-            const td = document.createElement(rowIndex === 0 ? 'th' : 'td'); // Use 'th' for the first row (headers)
-            td.textContent = element;
-            tr.appendChild(td);
-        });
+        eventDiv.setAttribute('style', 'grid-row: ' + (timeStringToFloat(event.VON) - 8) * 4 + "/" + (timeStringToFloat(event.BIS) - 8) * 4)
 
-        table.appendChild(tr);
+        let p1 = document.createElement('p');
+        p1.textContent = event.TITEL;
+        eventDiv.appendChild(p1)
+
+        let p2 = document.createElement('p');
+        p2.classList.add("time")
+        p2.textContent = (timeStringToFloat(event.VON) - 8) * 4 + " " + event.VON + " - " + event.BIS + " " + event.WOCHENTAG + " " + (timeStringToFloat(event.VON) - 8) * 4 + "/" + (timeStringToFloat(event.BIS) - 8) * 4;
+        eventDiv.appendChild(p2)
+
+        parent.appendChild(eventDiv);
     });
-} */
+
+
+    if (tableStyle.textContent != "") {
+        tableStyle.textContent = ""
+    }
+
+    var styles = ""
+
+    for (let i = 0; i < 8; i++) {
+        styles += ".time" + i + "{grid-row: " + i + "/" + (i + 1) + ";}"
+    }
+
+    let weekdays = ['MO', 'DI', 'MI', 'DO', 'FR']
+
+    weekdays.forEach((element, i) => {
+        i += 1
+        console.log(i);
+
+        styles += "." + element + "{grid-column: " + i + "/" + (i + 1) + ";}"
+    });
+
+    console.log(styles);
+
+    tableStyle.textContent = styles
+}
 
 function detectSeparator(csvText) {
     const separators = [',', ';', '\t', '|'];  // Common CSV separators
